1c3b77a7174c3014ac06c379070935a49e6b2d8a
Ach <bounane.ach@gmail.com>
2026-07-10 15:37:18 +0200

    front: allow optional public env defaults

--- front/src/lib/config/publicEnv.js
+++ front/src/lib/config/publicEnv.js
@@ -1,16 +1,12 @@
-import {
-	PUBLIC_API_BASE_URL,
-	PUBLIC_CLOUDFRONT_BASE_URL,
-	PUBLIC_USE_MOCK_GALLERY
-} from '$env/static/public';
+import { env } from '$env/dynamic/public';
 
 const trimTrailingSlash = (value) => value.replace(/\/+$/, '');
 
-export const API_BASE_URL = trimTrailingSlash(PUBLIC_API_BASE_URL || '/api');
+export const API_BASE_URL = trimTrailingSlash(env.PUBLIC_API_BASE_URL || '/api');
 
-export const CLOUDFRONT_BASE_URL = trimTrailingSlash(PUBLIC_CLOUDFRONT_BASE_URL || '/');
+export const CLOUDFRONT_BASE_URL = trimTrailingSlash(env.PUBLIC_CLOUDFRONT_BASE_URL || '/');
 
-export const USE_MOCK_GALLERY = PUBLIC_USE_MOCK_GALLERY === 'true';
+export const USE_MOCK_GALLERY = env.PUBLIC_USE_MOCK_GALLERY === 'true';
 
 export function buildApiUrl(path = '') {
 	const normalizedPath = path.startsWith('/') ? path : `/${path}`;
